137b5379fada1ac13312f829d3a40882dbe7269b
VictorAlejandro09 <vv21-0070@unphu.edu.do>
2024-08-19 16:16:10 -0400

    Add project files.

---
+++ Controllers/EstudianteController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practica1.Models;
+
+namespace Practica1.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        public IActionResult Index()
+        {
+            var estudiante = new Estudiante
+            {
+                Nombre = "Victor Alejandro",
+                Apellido = "Vargas Fuster",
+                CodigosAsignaturas = new List<string>
+                {
+                    "INF-422-02",
+                    "INF-556-01",
+                    "INF-576-01",
+                    "MAT-132-01"
+                },
+                Hobbies = new List<string>
+                {
+                    "Jugar videojuegos",
+                    "Jugar basketball",
+                    "Leer"
+                }
+            };
+
+            return View(estudiante);
+        }
+    }
+}
---
+++ Models/Estudiante.cs
@@ -0,0 +1,10 @@
+﻿namespace Practica1.Models
+{
+    public class Estudiante
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public List<string> CodigosAsignaturas { get; set; }
+        public List<string> Hobbies { get; set; }
+    }
+}
